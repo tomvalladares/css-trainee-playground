9fc4e7f83505a45cad4f9af13de45740592567bd
tomvalladares <tomvalladares@gmail.com>
2021-04-15 17:27:07 -0400

    preparacion base archivo colores y fonts

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -8,24 +8,42 @@ export default {
 
 	name: "Home",
 
-	data: () => ({ text: "Hello World from Home!" })
+	data: () => ({ text: "En un mundo imaginario arriba aparecerá un texto y abajo aparecerá otro" })
 }
 </script>
 
 <template lang="pug">
 
-.home
+.home.d-grid
 
-	p {{ text }}
 
-	router-link(to="/section") Go to Section
+
+
+	p.open-sans.text-center {{ text }}
+	p.open-sans.text-center 01
+	p.open-sans.text-center 02
+	p.open-sans.text-center 03
+
+	router-link(to="/section").button Go to Section
 
 </template>
 
 <style lang="stylus" scoped>
+@require ('../stylus/color.styl')
+
+.button
+	background 	color-button
+	color 		color-button-text
+	text-align  center
 
 .home
-	padding 20px
-	color blue
+	grid-template-columns 	1fr 1fr 1fr
+	grid-template-rows    	1fr 1fr 1fr
+	height 					100vh
+	padding 				50px
+
+p
+	color 		color-paragraph
+
 
 </style>
